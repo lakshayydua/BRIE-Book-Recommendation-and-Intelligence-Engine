a7261b0f37e5653555f268e32d7a9c51f85914b5
Lakshayy Dua <lakshayy.dua@here.com>
2018-12-31 10:55:18 +0100

    hero

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,8 +1,4 @@
 # This is buildpack-run.sh
 echo "Hello World"
 
-apt-get update
-apt-get install python3.6
-apt-get install python3-pip
-python3 --version
-pip3 --version
+apt-get install libmysqlclient-dev
