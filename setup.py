ca5f6a6485157801988fdc81c92002da871d93e1
Lakshayy Dua <lakshayy.dua@here.com>
2018-12-31 13:02:15 +0100

    heroku

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ python setup.py clean
 from setuptools import setup, find_packages, Command
 from codecs import open
 from os import path, system
-import logging
 
 here = path.abspath(path.dirname(__file__))
 
@@ -16,15 +15,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        system('rm -vrf ./dist ./*.pyc ./*.tgz ./*.egg-info')
 
 setup(
 
@@ -57,10 +47,5 @@ setup(
     # To export all data files (non .py files) inside utils directory.
     # Since utils contains __init__.py, a utils package directory will be created inside /usr/local/lib/python2.7
     # This allows data files such as config.json to be included in /usr/local/lib/python2.7/utils which is used by other programs
-    package_data={'utils': ['*']},
-    include_package_data=True,
-
-    cmdclass={
-            'clean': CleanCommand,
-        }
+    
 )
