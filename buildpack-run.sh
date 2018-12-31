ad05597c5cade6473cd8a3b9cf4557f825467ab2
Lakshayy Dua <lakshayy.dua@here.com>
2018-12-31 10:52:25 +0100

    hero

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,8 +1,8 @@
 # This is buildpack-run.sh
 echo "Hello World"
-python3 --version
+
+apt-get update
 apt-get install python3.6
-pip3 --version
 apt-get install python3-pip
 python3 --version
 pip3 --version
