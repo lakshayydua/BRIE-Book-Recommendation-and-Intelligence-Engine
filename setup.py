011747e5e0166296b09c4462629f9f516aa72f5c
Lakshayy Dua <lakshayy.dua@here.com>
2018-12-31 13:06:43 +0100

    heroku

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
                         'django',
                         'pymysql',
                         #'pymongo[tls,srv]',
-                        'mysqlclient',
+                        #'mysqlclient',
                         'plotly',
                         'numpy',
                         'pandas',
