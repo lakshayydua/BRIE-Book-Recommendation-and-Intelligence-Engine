7de642d7c63942ad2dca7a89f70b3b896a9baa47
Lakshayy Dua <lakshayy.dua@here.com>
2018-12-31 12:52:56 +0100

    heroku

--- buildpack-run.sh
+++
@@ -1,5 +0,0 @@
-# This is buildpack-run.sh
-echo "Hello World"
-
-pip3 install . 
-cd Brie ; pwd ; python3 manage.py runserver
--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
                         'pandas',
                         'pillow',
                         'nltk',
+                        'gunicorn',
                         ''
                         ],  # Optional
 
