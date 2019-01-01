95628ae916a5c7a9b3c1b7b9d77e12350899ec64
Lakshayy Dua <lakshayy.dua@here.com>
2019-01-01 03:40:31 +0100

    heroku

--- Brie/manage.py
+++ Brie/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Brie.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Brie.Brie.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
