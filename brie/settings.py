ceaceea07f84d3bce10728e46afc53305753f2f0
Lakshayy Dua <lakshayy.dua@here.com>
2019-01-01 06:59:06 +0100

    heroku allow host

--- brie/settings.py
+++ brie/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = 'qxs#*o*b96jv5qeelb9a4k+0w^ve!!dpzpb@$!7d)8*y2)-sw+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
