afb57a371ff4dccb9165e254b7635d11c1e2f429
Lakshayy Dua <lakshayy.dua@here.com>
2019-01-01 06:02:47 +0100

    heroku

--- brie/brie/settings.py
+++ brie/brie/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from django.apps import AppConfig
 
-#   from Brie.app.apps import AppConfig
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
