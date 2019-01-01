35e4aa29a226d9a8c9c8c8356ee88b3de1f96702
Lakshayy Dua <lakshayy.dua@here.com>
2019-01-01 04:39:15 +0100

    heroku

---
+++ Brie/app/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'Brie.app.apps.AppConfig'
--- Brie/app/apps.py
+++ Brie/app/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'app'
+
+#rock_n_roll/apps.py
+# from django.apps import AppConfig
+
+
+# class RockNRollConfig(AppConfig): # Our app config class
+#     name = 'rock_n_roll'
+#     verbose_name = "Rock ’n’ roll"
