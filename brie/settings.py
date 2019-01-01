b2d71658403a2d54fd34a277d92ade96dc81d52e
Lakshayy Dua <lakshayy.dua@here.com>
2019-01-01 09:36:23 +0100

    Commented STATICFILES_DIRS

--- brie/settings.py
+++ brie/settings.py
@@ -153,8 +153,9 @@ STATICFILES_FINDERS = (
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '..', 'static'),
-)
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '..', 'static'),
+# )
