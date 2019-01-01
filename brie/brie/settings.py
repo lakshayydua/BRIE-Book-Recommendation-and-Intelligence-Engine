750412c5807078862903daff17e2026fe592a3ea
Lakshayy Dua <lakshayy.dua@here.com>
2019-01-01 07:28:39 +0100

    heroku added support for static files

--- brie/brie/settings.py
+++ brie/brie/settings.py
@@ -149,8 +149,8 @@ STATICFILES_FINDERS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     #os.path.join(BASE_DIR, '..', 'static'),
